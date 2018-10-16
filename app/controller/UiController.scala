744e7f5e8cb74aab96784325143a6a6ca93721c6
gregor <gregor_alias1@outlook.de>
2018-10-16 19:22:04 +0200

    copy as is

--- app/controller/UiController.scala
+++ app/controller/UiController.scala
@@ -5,5 +5,9 @@ import javax.inject.Inject
 import play.api.mvc.InjectedController
 
 class UiController @Inject()(assets: Assets) extends InjectedController {
+  def index() = Action {
+    Redirect("index.html")
+  }
+
   def asset(path: String) = assets.at(path)
 }
