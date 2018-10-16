66766e9c801639dbed7998b074612b27618aec83
gregor <gregor_alias1@outlook.de>
2018-10-16 20:16:24 +0200

    downloaded all linked assets

--- app/controller/UiController.scala
+++ app/controller/UiController.scala
@@ -3,11 +3,16 @@ package controller
 import controllers.Assets
 import javax.inject.Inject
 import play.api.mvc.InjectedController
+import play.twirl.api.Html
 
 class UiController @Inject()(assets: Assets) extends InjectedController {
-  def index() = Action {
-    Redirect("index.html")
-  }
+  private def ui(html:Html) = Action(Ok(html))
+
+  def index() = ui(views.html.index())
+  def chorleiter() = ui(views.html.chorleiter())
+  def kontakt() = ui(views.html.kontakt())
+  def konzerte() = ui(views.html.konzerte())
+  def programm() = ui(views.html.programm())
 
   def asset(path: String) = assets.at(path)
 }
