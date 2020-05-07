8759e0c5b90bd2ef88c3d2f15ee74a298ee382a4
Gregor Ihmor <gregor.ihmor@gmail.com>
2020-05-07 12:58:28 +0200

    service worker: wait 30 sec before caching all resources

--- src/main/static/service-worker.js
+++ src/main/static/service-worker.js
@@ -83,27 +83,37 @@ var appShellFiles = [
   "/",
 ];
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  console.trace('[Service Worker] Install');
   e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching all: app shell');
-      return cache.addAll(appShellFiles);
+    sleep(30 * 1000).then(() => {
+      console.trace('[Service Worker] caching all resources');
+      caches.open(cacheName).then((cache) => cache.addAll(appShellFiles))
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
+    caches.match(e.request).then((cachedResponse) =>
+       cachedResponse || fetchAndCache(request)
+    )
   );
 });
+
+function fetchAndCache(request) {
+  let responsePromise = fetch(e.request)
+  let cachePromise = caches.open(cacheName)
+
+  responsePromise.then((response) => {
+    cachePromise.then((cache) => {
+        console.trace('[Service Worker] Caching new resource: '+ request.url);
+        cache.put(request, response.clone());
+      });
+  });
+  return responsePromise;
+}
